b80c6592a19a928fa6b16a96680991494bf5425b
Paw in Data <59799041+PawinData@users.noreply.github.com>
2020-05-05 05:57:58 +0200

    run in lab

---
+++ Centers.py
@@ -0,0 +1,46 @@
+import json
+from random import sample
+from scipy.optimize import differential_evolution
+from sklearn.feature_extraction.text import TfidfVectorizer
+from scipy.sparse import save_npz
+from scipy.sparse.linalg import svds
+
+k = 500
+
+Airline = pd.read_csv("https://drive.google.com/uc?export=download&id=1HruH1PQRPPPwYvYvNZzjHpR1rZNFvUWN", encoding="utf-8")
+
+Airline = Airline[["tweet_id", "airline_sentiment", "text"]]
+Airline.airline_sentiment = Airline.airline_sentiment.replace({"negative":0, "positive":1, "neutral":2})
+
+ID_train = json.load(open())  # load ID of training set
+Train = Airline.iloc[ID_train]
+Test = Airline.iloc[ID_test]
+Train = Train.sort_values(by=["airline_sentiment"])
+truth = Train["airline_sentiment"].tolist()
+
+vectorizer = TfidfVectorizer(stop_words="english", max_features=100000, max_df=0.5, smooth_idf=True)
+Y = vectorizer.fit_transform(Train["text"])
+M,N = Y.shape
+
+counts = dict()
+for sentiment in range(3):
+    data = Train.loc[Train["airline_sentiment"]==sentiment]
+    counts[sentiment] = data.shape[0]
+print(counts)
+
+accum = {-1:0, 0:counts[0], 1:counts[0]+counts[1], 2:counts[0]+counts[1]+counts[2]}
+
+UU, SIGMA, VV_T = svds(Y, k)
+SIGMA = np.diag(SIGMA)
+PP = UU.dot(np.diag([norm(VV_T[j,:])**2 for j in range(k)]))
+
+Centers = dict()
+Bounds = [(min(PP[:,j]), max(PP[:,j])) for j in range(k)]
+for sentiment in range(3):
+    res = differential_evolution(obj_func, bounds=Bounds, args=(PP,accum,sentiment), maxiter=100000)
+    Centers[sentiment] = res.x 
+    # numpy arrays --> lists for storing in json file
+    Centers[sentiment] = [float(ele) for ele in Centers[sentiment]]  
+    
+with open("Centers.json", "w") as file:
+    file.write(json.dumps(Centers, indent=4))
