cd12fd1c25dfcb76c244d4321b339b90c0d40515
Paw in Data <59799041+PawinData@users.noreply.github.com>
2020-05-07 01:15:33 +0200

    run in lab

--- Centers.py
+++ Centers.py
@@ -1,60 +1,68 @@
-import json
 import numpy as np
-import pandas as pd
-from random import sample
-from scipy.optimize import differential_evolution
-from sklearn.feature_extraction.text import TfidfVectorizer
+import json
+from numpy.linalg import norm
 from scipy.sparse.linalg import svds
-from numpy.linalg import inv, norm
+from scipy.optimize import minimize
 
 k = 500
 
-Airline = pd.read_csv("https://drive.google.com/uc?export=download&id=1HruH1PQRPPPwYvYvNZzjHpR1rZNFvUWN", encoding="utf-8")
 
+Airline = pd.read_csv("https://drive.google.com/uc?export=download&id=1HruH1PQRPPPwYvYvNZzjHpR1rZNFvUWN", encoding="utf-8")
 Airline = Airline[["tweet_id", "airline_sentiment", "text"]]
 Airline.airline_sentiment = Airline.airline_sentiment.replace({"negative":0, "positive":1, "neutral":2})
+print("Airline Dataset loaded")
 
-ID_train = json.load(open("ID_train.json"))  # load ID of training set
-Train = Airline.iloc[ID_train]
 
-Train = Train.sort_values(by=["airline_sentiment"])
-truth = Train["airline_sentiment"].tolist()
+Y = load("Document_Term_Airline.npz")
+print("Document Term matrix loaded.")
 
-vectorizer = TfidfVectorizer(stop_words="english", max_features=100000, max_df=0.5, smooth_idf=True)
-Y = vectorizer.fit_transform(Train["text"])
-M,N = Y.shape
-
-counts = dict()
-for sentiment in range(3):
-    data = Train.loc[Train["airline_sentiment"]==sentiment]
-    counts[sentiment] = data.shape[0]
-print(counts)
-
-accum = {-1:0, 0:counts[0], 1:counts[0]+counts[1], 2:counts[0]+counts[1]+counts[2]}
-
-UU, SIGMA, VV_T = svds(Y, k)
-SIGMA = np.diag(SIGMA)
+UU, SIGMA, VV_T = svds(Y,k)
+print("SVD done")
 PP = UU.dot(np.diag([norm(VV_T[j,:])**2 for j in range(k)]))
-print("SVD is done.")
+print("low-rank representation matrix calculated.")
 
-def obj_func(center, P, ACCUM, sentiment):
-    d = 0
-    for row in range(ACCUM[sentiment-1], ACCUM[sentiment]):
-        v = P[row,:]
-        d += v.dot(center)/(norm(v)*norm(center))
-    return(-d)
-    
-print("Objective Function is defined.")
-    
-Centers = dict()
-Bounds = [(min(PP[:,j]), max(PP[:,j])) for j in range(k)]
+with open("ID_train.json", "r") as fhand:
+    ID_train = json.load(fhand)
+print("ID_train loaded")
+
+# compute Centroids as initial values to calculate Cosine-Similarity Centers
+Centroids = dict()
+counts = dict()
+for id in ID_train:
+    sentiment = Airline["airline_sentiment"][id]
+    counts[sentiment] = counts.get(sentiment,0) + 1
+    Centroids[sentiment] = Centroids.get(sentiment,0) + PP[id,:]
 for sentiment in range(3):
-    print("start an optimization.")
-    res = differential_evolution(obj_func, bounds=Bounds, args=(PP,accum,sentiment), maxiter=500)
-    print("an optimization is done")
-    Centers[sentiment] = res.x 
-    # numpy arrays --> lists for storing in json file
-    Centers[sentiment] = [float(ele) for ele in Centers[sentiment]]  
+    Centroids[sentiment] /= counts[sentiment]
+print("Centroids calculated")
+
+# split training set by 3 sentiment, only store ID
+Train_ID = {0:[], 1:[], 2:[]}
+for id in ID_train:
+    sentiment = Airline["airline_sentiment"][id]
+    Train_ID[sentiment].append(id)
+print("3 lists of ID split by sentiment")
+
+
+# input: array P, each row = low-rank representation of a document
+# input: sentiment = all rows in P are labeled with
+# input: arbitrary center = to be optimized with respect to
+# output: sum of cosine similarity between center and all rows in P
+def obj_f(center, P, sentiment):
+    d = 0
+    mg = norm(center)
+    for row in range(P.shape[0]):
+        v = P[row,:]
+        d += v.dot(center) / (norm(v)*mg)
+    return(-d / P.shape[0])
+print("objective function defined")
+
+Centers = dict()
+for sentiment in range(3):
+    print("start an optimization")
+    result = minimize(obj_f, x0=Centroids[sentiment], args=(PP[Train_ID[sentiment,], sentiment),)
+    Centers[sentiment] = result.x
+    Centers[sentiment] = [ele for ele in Centers[sentiment]]
+    print("end an optimization")
     
-with open("Centers.json", "w") as file:
-    file.write(json.dumps(Centers, indent=4))
+json.dump(Centers, open("Centers.json", "w"))
